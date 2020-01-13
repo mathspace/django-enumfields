169609c9f37914430c8481df85a66660af6e8750
Brandon Navra <brandon.navra@gmail.com>
2020-01-13 12:42:44 +1100

    Django 3.0 compatability

--- setup.py
+++ setup.py
@@ -56,6 +56,9 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
     ],
+    install_requires=[
+        "six"
+    ],
     tests_require=[
         'pytest-django',
         'Django',
